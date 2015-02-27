bbf30b94a2bf69733fe21d3facd306f05deccabb
Marina <My@Marina.local>
2015-02-27 23:23:09 +0300

    Menu - settings, about. Activity about. Settings for new language activity.

--- app/src/main/java/com/example/kic/translator/MainActivity.java
+++ app/src/main/java/com/example/kic/translator/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.kic.translator;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -44,13 +45,14 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.main_action_settings) {
+            return true;
+        }
+        if (id == R.id.main_action_about) {
+            Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+            startActivity(intent);
             return true;
         }
 
